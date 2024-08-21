6930cbee5d28de82d91a5427c4edfc2e74b5ceba
Szeles Dániel <mr.szeles97@gmail.com>
2024-08-21 12:14:54 +0200

    add more init data for sql

--- demo/src/main/resources/sql/data.sql
+++ demo/src/main/resources/sql/data.sql
@@ -2,3 +2,108 @@ INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (1, 'Kis Pista', '1990-0
 INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
     VALUES (1, 1, 'állandó', 'Nagy utca 1', 'Budapest', 'Pest', 'Magyarország', '1111');
 INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (1, 1, 'email', 'kispista@gmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (2, 'Nagy János', '1985-05-15');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (2, 2, 'állandó', 'Kis utca 5', 'Debrecen', 'Hajdú-Bihar', 'Magyarország', '4025');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (2, 2, 'email', 'nagyjanos@hotmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (3, 'Szabó Tímea', '1992-08-20');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (3, 3, 'állandó', 'Petőfi utca 10', 'Szeged', 'Csongrád', 'Magyarország', '6720');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (3, 3, 'telefon', '+36-30-123-4567');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (4, 'Kovács Béla', '1979-12-30');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (4, 4, 'állandó', 'Rákóczi utca 20', 'Pécs', 'Baranya', 'Magyarország', '7621');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (4, 4, 'email', 'kovacs.bela@yahoo.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (5, 'Tóth Gábor', '1988-03-10');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (5, 5, 'ideiglenes', 'Kossuth tér 3', 'Győr', 'Győr-Moson-Sopron', 'Magyarország', '9021');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (5, 5, 'telefon', '+36-70-987-6543');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (6, 'Varga Eszter', '1995-06-25');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (6, 6, 'állandó', 'Fő utca 15', 'Miskolc', 'Borsod-Abaúj-Zemplén', 'Magyarország', '3525');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (6, 6, 'email', 'varga.eszter@freemail.hu');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (7, 'Fekete Anna', '1991-11-11');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (7, 7, 'ideiglenes', 'Arany János utca 8', 'Székesfehérvár', 'Fejér', 'Magyarország', '8000');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (7, 7, 'telefon', '+36-20-222-3333');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (8, 'Németh László', '1982-02-14');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (8, 8, 'állandó', 'József Attila utca 12', 'Nyíregyháza', 'Szabolcs-Szatmár-Bereg', 'Magyarország', '4400');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (8, 8, 'email', 'nemeth.laszlo@gmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (9, 'Horváth Zoltán', '1987-07-07');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (9, 9, 'ideiglenes', 'Bartók Béla utca 22', 'Kecskemét', 'Bács-Kiskun', 'Magyarország', '6000');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (9, 9, 'telefon', '+36-30-456-7890');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (10, 'Molnár Ilona', '1993-09-30');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (10, 10, 'állandó', 'Dózsa György út 5', 'Tatabánya', 'Komárom-Esztergom', 'Magyarország', '2800');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (10, 10, 'email', 'molnar.ilona@gmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (11, 'Takács Attila', '1980-04-18');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (11, 11, 'állandó', 'Béke tér 9', 'Eger', 'Heves', 'Magyarország', '3300');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (11, 11, 'telefon', '+36-70-111-2222');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (12, 'Balogh András', '1996-12-12');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (12, 12, 'ideiglenes', 'Ady Endre utca 16', 'Zalaegerszeg', 'Zala', 'Magyarország', '8900');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (12, 12, 'email', 'balogh.andras@freemail.hu');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (13, 'Hegedűs Erika', '1989-10-25');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (13, 13, 'állandó', 'Munkácsy Mihály utca 3', 'Szekszárd', 'Tolna', 'Magyarország', '7100');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (13, 13, 'telefon', '+36-20-333-4444');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (14, 'Kiss Ferenc', '1983-03-05');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (14, 14, 'állandó', 'Váci utca 7', 'Sopron', 'Győr-Moson-Sopron', 'Magyarország', '9400');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (14, 14, 'email', 'kiss.ferenc@citromail.hu');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (15, 'Farkas Tamás', '1998-07-22');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (15, 15, 'ideiglenes', 'Széchenyi tér 6', 'Szolnok', 'Jász-Nagykun-Szolnok', 'Magyarország', '5000');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (15, 15, 'telefon', '+36-30-666-7777');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (16, 'Papp Zsuzsanna', '1991-05-17');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (16, 16, 'állandó', 'Bem József utca 4', 'Veszprém', 'Veszprém', 'Magyarország', '8200');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (16, 16, 'email', 'papp.zsuzsanna@gmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (17, 'Gál László', '1986-06-09');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (17, 17, 'ideiglenes', 'Szabadság utca 11', 'Békéscsaba', 'Békés', 'Magyarország', '5600');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (17, 17, 'telefon', '+36-70-888-9999');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (18, 'Bíró Katalin', '1994-09-13');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (18, 18, 'állandó', 'Hunyadi utca 2', 'Kaposvár', 'Somogy', 'Magyarország', '7400');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (18, 18, 'email', 'biro.katalin@gmail.com');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (19, 'Oláh Péter', '1981-11-23');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (19, 19, 'állandó', 'Rózsa utca 17', 'Szombathely', 'Vas', 'Magyarország', '9700');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (19, 19, 'telefon', '+36-20-555-6666');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (20, 'Juhász Csaba', '1987-12-01');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (20, 20, 'állandó', 'Vörösmarty utca 13', 'Gyula', 'Békés', 'Magyarország', '5700');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (20, 20, 'email', 'juhasz.csaba@freemail.hu');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (21, 'Lukács Viktória', '1993-04-28');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (21, 21, 'állandó', 'Kinizsi utca 18', 'Baja', 'Bács-Kiskun', 'Magyarország', '6500');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (21, 21, 'telefon', '+36-30-777-8888');
+
+INSERT INTO Szemelyek (id, nev, szuletesi_datum) VALUES (22, 'Vincze József', '1984-07-14');
+INSERT INTO Cimek (id, szemely_id, cim_tipus, utca, varos, allam, orszag, iranyitoszam)
+    VALUES (22, 22, 'ideiglenes', 'Kossuth Lajos utca 20', 'Hódmezővásárhely', 'Csongrád', 'Magyarország', '6800');
+INSERT INTO Elerhetosegek (id, szemely_id, tipus, ertek) VALUES (22, 22, 'email', 'vincze.jozsef@gmail.com');
